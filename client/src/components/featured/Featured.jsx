b455f142f784c2afa2cd02c90f0d329dea5942cc
Shoujanya <jptmax123@gmail.com>
2025-10-18 20:32:15 +0545

    fix

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -5,6 +5,7 @@ const Featured = () => {
   const { data, loading, error } = useFetch(
     "/api/hotels/countByCity?cities=berlin,madrid,london"
   );
+  console.log("API URL:", process.env.REACT_APP_API_URL);
 
   const cities = [
     {
