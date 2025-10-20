7298434154f94cab2e1ebaf2819483e632f760df
Shoujanya <jptmax123@gmail.com>
2025-10-20 11:06:18 +0545

    Vite fix

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,18 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    open: true,
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: process.env.VITE_API_URL || "http://localhost:8800", // fallback to local backend if env not set
-        changeOrigin: true,
-        secure: false, // set true if backend uses HTTPS
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    server: {
+      open: true,
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL || "http://localhost:8800",
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
