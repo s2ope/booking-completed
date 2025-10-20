58aa1fb9abff213d912926981ad73f83edd8f5d0
Shoujanya <jptmax123@gmail.com>
2025-10-20 12:15:35 +0545

    Url fix

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -26,9 +26,7 @@ const Hotel = () => {
   const [open, setOpen] = useState(false);
   const [openModal, setOpenModal] = useState(false);
 
-  const { data, loading, error } = useFetch(
-    `http://localhost:8800/api/hotels/find/${id}`
-  );
+  const { data, loading, error } = useFetch(`/api/hotels/find/${id}`);
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
 
